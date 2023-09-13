d648e2599420b2f551d4b2d8761e7188888dfe47
Joe Boudreau <jboudreau@squareup.com>
2023-09-13 09:50:10 -0400

    added api key config. more side panel stuff

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,6 @@
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
 // Allows users to open the side panel by clicking on the action toolbar icon
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
@@ -6,7 +9,12 @@ chrome.sidePanel
 function setupContextMenu() {
   chrome.contextMenus.create({
     id: 'summarize-text',
-    title: 'summarize',
+    title: 'Summarize',
+    contexts: ['selection']
+  });
+  chrome.contextMenus.create({
+    id: 'translate-text',
+    title: 'Translate',
     contexts: ['selection']
   });
 }
@@ -15,9 +23,19 @@ chrome.runtime.onInstalled.addListener(() => {
   setupContextMenu();
 });
 
-chrome.contextMenus.onClicked.addListener((data) => {
-  chrome.runtime.sendMessage({
-    name: 'summarize-text',
-    data: { value: data.selectionText }
-  });
+chrome.contextMenus.onClicked.addListener(async (data, tab) => {
+  await chrome.sidePanel.open({tabId: tab.id});
+  // Wait a little bit for the message listener to be initialized in the side panel JS (if it was just opened)
+  await sleep(500);
+  if (data.menuItemId === 'summarize-text') {
+    chrome.runtime.sendMessage({
+      name: 'summarize-text',
+      data: { value: data.selectionText }
+    });
+  } else if (data.menuItemId === 'translate-text') {
+    chrome.runtime.sendMessage({
+      name: 'translate-text',
+      data: { value: data.selectionText }
+    });
+  }
 });
--- sidepanel.js
+++ sidepanel.js
@@ -1,10 +1,9 @@
-const OPENAI_API_KEY="OPEN_API_KEY_HERE";
-
 async function getPromptCompletion(prompt) {
     try {
+      const openAiApiKey = await getAPIKey();
       const headers = new Headers();
       headers.append("Content-Type", "application/json");
-      headers.append("Authorization", `Bearer ${OPENAI_API_KEY}`);
+      headers.append("Authorization", `Bearer ${openAiApiKey}`);
       const body = {
         model: "gpt-3.5-turbo",
         temperature: 0.2,
@@ -38,12 +37,27 @@ async function getPromptCompletion(prompt) {
     }
   }
 
+  async function getAPIKey() {
+    const result = await chrome.storage.local.get('openAiApiKey');
+    if (!result.openAiApiKey) {
+      // TODO show error and offer link to options config
+    }
+    return result.openAiApiKey;
+  }
+
 chrome.runtime.onMessage.addListener(async ({ name, data }) => {
   if (name === 'summarize-text') {
     openPanel(document.getElementById("summarize-acc"))
     const summarizePrompt = `Please summarize the following text in 2 or 3 sentences. Don't worry about the context, just try to rephrase the text more concisely. 2 or 3 sentences at most. Text: """ ${data.value} """`
     const response = await getPromptCompletion(summarizePrompt);
-    document.getElementById("responseArea").textContent = response;
+    //const response = "Even if you could hypothetically build a constantly updating database of all of the untold billions of pages on the internet, the storage and bandwidth costs alone would bankrupt practically any company on the planet. And thatâ€™s not even counting the cost of searching that database millions or billions of times a day. Add in the fact that every millisecond matters â€” Google still advertises how long every query took at the top of your results â€” and you donâ€™t have time to look over the whole database, anyway.Even if you could hypothetically build a constantly updating database of all of the untold billions of pages on the internet, the storage and bandwidth costs alone would bankrupt practically any company on the planet. And thatâ€™s not even counting the cost of searching that database millions or billions of times a day. Add in the fact that every millisecond matters â€” Google still advertises how long every query took at the top of your results â€” and you donâ€™t have time to look over the whole database, anyway.";
+    document.getElementById("summarize-output").textContent = response;
+    const panel = document.getElementById("summarize-panel");
+    // Reset the panel height in case the response overflows
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+  else if (name === 'translate-text') {
+    // TODO
   }
 });
 
@@ -54,6 +68,7 @@ for (i = 0; i < acc.length; i++) {
 }
 
 function openPanel(accordianButton) {
+  console.log('opening panel: ' + accordianButton.id)
   accordianButton.classList.toggle("active");
   var panel = accordianButton.nextElementSibling;
   if (panel.style.maxHeight) {
